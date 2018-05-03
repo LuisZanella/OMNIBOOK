4b7c6a349165692eda94db16340d6100ea8a6909
LuisZanella <luiscontreraszane@gmail.com>
2018-05-02 23:57:59 -0500

    Actualizacion de Atributos de clases para la base de datos Modulos

--- App_Code/modulo/UsuarioModelo.cs
+++ App_Code/modulo/UsuarioModelo.cs
@@ -11,10 +11,15 @@ public class UsuarioModelo
         public long Id_Usuario { get; set; }
         public string Nombre { get; set; }
         public string Nick { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Correo { get; set; }
+        public string Contrasenia { get; set; }
         public string Fecha_Nacimiento { get; set; }
         public bool Estatus { get; set; }
+        public string Imagen_Perfil { get; set; }
+        public string Imagen_Portada { get; set; }
+        public string Telefono { get; set; }
+        public string Telefono_Casa { get; set; }
+        public string Biografia { get; set; }
 
     //
     // TODO: Add constructor logic here
