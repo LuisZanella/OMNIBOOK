66f366d5ea36e2f8105dbeed081c908d5c5611af
LuisZanella <luiscontreraszane@gmail.com>
2018-05-31 11:31:42 -0500

    Base de datos actualizada

--- BaseData.sql
+++ BaseData.sql
@@ -1,10 +1,12 @@
 CREATE DATABASE omnitrix
 GO
+
+
 USE omnitrix
 GO
 
 CREATE TABLE Usuario(
-Id_Usuario INT IDENTITY (1,1) PRIMARY KEY,
+Id_Usuario BIGINT IDENTITY (1,1) PRIMARY KEY,
 Nombre NVARCHAR(50) UNIQUE NOT NULL,
 Nick NVARCHAR(50) UNIQUE NOT NULL,
 Correo NVARCHAR(40) UNIQUE NOT NULL,
@@ -12,38 +14,57 @@ Contrasenia NVARCHAR(200) NOT NULL,
 Fecha_Nacimiento DATE,
 Verificacion_Clave NVARCHAR(300),
 Estatus BIT DEFAULT '0',
+)
+GO
+CREATE TABLE Datos(
+Id_Datos BIGINT IDENTITY (1,1) PRIMARY KEY,
+Id_Usuario BIGINT FOREIGN KEY REFERENCES Usuario(Id_Usuario) NOT NULL,
+Vocacion NVARCHAR(60),
+Ciudad NVARCHAR(30),
+Pais NVARCHAR(40),
+Estado NVARCHAR(200),
+Seguidores BIGINT,
+Siguiendo BIGINT,
+NombreArtistico NVARCHAR(100),
 Imagen_Perfil NVARCHAR(80),
 Imagen_Portada NVARCHAR(80),
 Telefono NVARCHAR(20),
 Telefono_Casa NVARCHAR(20),
-Biografia NVARCHAR(300),
-
+Biografia NVARCHAR(300)
 )
 GO
 CREATE TABLE Amistad(
-Id_Amistad INT PRIMARY KEY IDENTITY (1,1),
-Id_Usuario INT FOREIGN KEY REFERENCES Usuario(Id_Usuario) NOT NULL,
-Id_Usuario_Dos INT FOREIGN KEY REFERENCES Usuario(Id_Usuario) NOT NULL
+Id_Amistad BIGINT PRIMARY KEY IDENTITY (1,1),
+Id_Usuario BIGINT FOREIGN KEY REFERENCES Usuario(Id_Usuario) NOT NULL,
+Id_Usuario_Dos BIGINT FOREIGN KEY REFERENCES Usuario(Id_Usuario) NOT NULL
 )
 GO
 
 CREATE TABLE Publicacion(
-Id_Publicacion INT IDENTITY PRIMARY KEY,
-Id_Usuario INT FOREIGN KEY REFERENCES Usuario(Id_Usuario) NOT NULL,
-Id_Amistad INT FOREIGN KEY REFERENCES Amistad(Id_Amistad),
+Id_Publicacion BIGINT IDENTITY PRIMARY KEY,
+Id_Usuario BIGINT FOREIGN KEY REFERENCES Usuario(Id_Usuario) NOT NULL,
+Id_Amistad BIGINT FOREIGN KEY REFERENCES Amistad(Id_Amistad),
 Titulo NVARCHAR(150),
 Imagen NVARCHAR(80),
+Tipo INT,
+Descripcion NVARCHAR(300),
+Fuente NVARCHAR(200) NULL,
 Fecha_Publicacion DATETIME NOT NULL
 )
 GO
 CREATE TABLE Comentario(
-Id_Comentario INT IDENTITY (1,1) PRIMARY KEY,
-Id_Usuario INT FOREIGN KEY REFERENCES Usuario(Id_Usuario) NOT NULL,
-Id_Publicacion INT FOREIGN KEY REFERENCES Publicacion(Id_Publicacion) NOT NULL,
+Id_Comentario BIGINT IDENTITY (1,1) PRIMARY KEY,
+Id_Usuario BIGINT FOREIGN KEY REFERENCES Usuario(Id_Usuario) NOT NULL,
+Id_Publicacion BIGINT FOREIGN KEY REFERENCES Publicacion(Id_Publicacion) NOT NULL,
 Comentario NVARCHAR(260)
 )
 GO
-
+CREATE TABLE Notificacion(
+Id_Notificacion BIGINT IDENTITY (1,1) PRIMARY KEY,
+Id_Publicacion BIGINT FOREIGN KEY REFERENCES Publicacion(Id_Publicacion) NOT NULL,
+Estatus INT DEFAULT '1'
+)
+GO
 CREATE PROCEDURE [dbo].[sp_UpNombre]
 @Variable NVARCHAR(50),
 @Id INT
