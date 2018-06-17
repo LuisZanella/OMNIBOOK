95b1f43dfc6e69d0c25f2952a8cb5f10da2a6f66
LuisZanella <luiscontreraszane@gmail.com>
2018-06-16 20:25:31 -0500

    Cosas corregidas y tipo 3 publicacion arreglada

--- recursos/js/principalPerfilTop.js
+++ recursos/js/principalPerfilTop.js
@@ -26,14 +26,13 @@ function Notificaciones() {
     
     var Obj = {
         "spNombre": spName, "Campos": Campos, "Id": IdUser
-    }
+    };
     var data = JSON.stringify(Obj);
     ajax("OmniService.asmx", "Leer", data, "CargarNotificaciones");
 }
 function CargarNotificaciones(Response) {
-    console.warn(Response);
     for (var i = 0; i < Response.d.length; i++) {
-            /*SPLISTO*/$("#notificaciones").append('<li class="list-group-item black lt box-shadow-z0 b"> <span class="pull-left m-r" ><img src="' + Response.d[i].Datos.Imagen_Perfil + '" alt="..." class="w-40 img-circle"></span><span class="clear block">' + Response.d[i].Usuario.Nick+' : ' + '<a href="" class="text-primary" >'+ Response.d[i].Publicacion.Descripcion+'</a > </br> <small class="text-muted">hace 10 minutos</small> </span > </li >'); //imagen, nombre de usuario y titulo
+            /*SPLISTO*/$("#notificaciones").append('<li class="list-group-item black lt box-shadow-z0 b"> <span class="pull-left m-r" ><img src="' + Response.d[i].Datos.Imagen_Perfil +'" alt="..." class="w-40 img-circle"></span><span class="clear block">' + Response.d[i].Usuario.Nick+' : ' + '<a href="" class="text-primary" >'+ Response.d[i].Publicacion.Descripcion+'</a > </br> <small class="text-muted">hace 10 minutos</small> </span > </li >'); //imagen, nombre de usuario y titulo
     }
 }
 //Poner la imagen del usuario
@@ -42,16 +41,15 @@ function ImagenPerfil() {
     var spName = "sp_obtenerImagenPerfil";
     var Obj = {
         "spNombre": spName, "Campos": Campos, "Id": IdUser
-    }
+    };
     var data = JSON.stringify(Obj);
     ajax("OmniService.asmx", "Leer", data, "CargarImagenPerfil");
 }
 function CargarImagenPerfil(Response) {
-    console.warn(Response);
     for (var i = 0; i < Response.d.length; i++) {
-        $("#imagenNBSD").append('<span class="avatar w-32"><img src="' + Response.d[i].Datos.ImagenPerfil + '" alt="..."><i class="on b-white bottom"></i></span>'); //imagen, nombre de usuario y titulo
-        $("#imagenNBSI").append('<img class=" w-40 img-circle" src="' + Response.d[i].Datos.ImagenPerfil + '" alt="."/>');
-        $("#imagenPrincipalPersona").append('<img src="' + Response.d[i].Datos.ImagenPerfil + '"> <i class="on b-white"></i>');
+        $("#imagenNBSD").append('<span class="avatar w-32"><img src="' + Response.d[i].Datos.Imagen_Perfil + '" alt="..."><i class="on b-white bottom"></i></span>'); //imagen, nombre de usuario y titulo
+        $("#imagenNBSI").append('<img class=" w-40 img-circle" src="' + Response.d[i].Datos.Imagen_Perfil + '" alt="."/>');
+        $("#imagenPrincipalPersona").append('<img src="' + Response.d[i].Datos.Imagen_Perfil + '"> <i class="on b-white"></i>');
     }
 }
 //Poner imagen portada
@@ -60,12 +58,11 @@ function ImagenPortada() {
     var spName = "sp_obtenerImagenPortadaPerfil";
     var Obj = {
         "spNombre": spName, "Campos": Campos, "Id": IdUser
-    }
+    };
     var data = JSON.stringify(Obj);
     ajax("OmniService.asmx", "Leer", data, "CargarImagenPortada");
 }
 function CargarImagenPortada(Response) {
-    console.warn(Response);
     for (var i = 0; i < Response.d.length; i++) {
         $("#imagenFondoPerfilPrincipal").append('<img src= "' + Response.d[i].Datos.Imagen_Portada + '" class="blur opacity-3"/>');
     }
@@ -76,12 +73,11 @@ function Nombre() {
     var spName = "sp_obtenerNombrePerfil";
     var Obj = {
         "spNombre": spName, "Campos": Campos, "Id": IdUser
-    }
+    };
     var data = JSON.stringify(Obj);
     ajax("OmniService.asmx", "Leer", data, "CargarNombre");
 }
 function CargarNombre(Response) {
-    console.warn(Response);
     for (var i = 0; i < Response.d.length; i++) {
         $("#nombreUsuario").append(Response.d[i].Usuario.Nombre);
     }
@@ -92,12 +88,11 @@ function Vocacion() {
     var spName = "sp_obtenerVocacionPerfil";
     var Obj = {
         "spNombre": spName, "Campos": Campos, "Id": IdUser
-    }
+    };
     var data = JSON.stringify(Obj);
     ajax("OmniService.asmx", "Leer", data, "CargarVocacion");
 }
 function CargarVocacion(Response) {
-    console.warn(Response);
     for (var i = 0; i < Response.d.length; i++) {
         $("#vocacionUsuario").append(Response.d[i].Datos.Vocacion);
         $("#vocacion").append('<small class="text-muted" >' + Response.d[i].Datos.Vocacion + '</small>');
@@ -109,12 +104,11 @@ function Ubicacion() {
     var spName = "sp_obtenerUbicacionPerfil";
     var Obj = {
         "spNombre": spName, "Campos": Campos, "Id": IdUser
-    }
+    };
     var data = JSON.stringify(Obj);
     ajax("OmniService.asmx", "Leer", data, "CargarUbicacion");
 }
 function CargarUbicacion(Response) {
-    console.warn(Response);
     for (var i = 0; i < Response.d.length; i++) {
         $("#ubicacionUsuario").append(Response.d[i].Datos.Ciudad + ", " + Response.d[i].Datos.Pais);
     }
@@ -125,12 +119,11 @@ function Estado() {
     var spName = "sp_obtenerEstadoPerfil";
     var Obj = {
         "spNombre": spName, "Campos": Campos, "Id": IdUser
-    }
+    };
     var data = JSON.stringify(Obj);
     ajax("OmniService.asmx", "Leer", data, "CargarEstado");
 }
 function CargarEstado(Response) {
-    console.warn(Response);
     for (var i = 0; i < Response.d.length; i++) {
         $("#estadoUsuario").append(Response.d[i].Datos.Estado);
     }
@@ -141,12 +134,11 @@ function Seguidores() {
     var spName = "sp_obtenerSeguidoresPerfil";
     var Obj = {
         "spNombre": spName, "Campos": Campos, "Id": IdUser
-    }
+    };
     var data = JSON.stringify(Obj);
     ajax("OmniService.asmx", "Leer", data, "CargarSeguidores");
 }
 function CargarSeguidores(Response) {
-    console.warn(Response);
     for (var i = 0; i < Response.d.length; i++) {
         $("#seguidores").append(Response.d[i].Datos.Seguidores);
     }
@@ -157,12 +149,11 @@ function Siguiendo() {
     var spName = "sp_obtenerSiguiendoPerfil";
     var Obj = {
         "spNombre": spName, "Campos": Campos, "Id": IdUser
-    }
+    };
     var data = JSON.stringify(Obj);
     ajax("OmniService.asmx", "Leer", data, "CargarSiguiendo");
 }
 function CargarSiguiendo(Response) {
-    console.warn(Response);
     for (var i = 0; i < Response.d.length; i++) {
         $("#siguiendo").append(Response.d[i].Datos.Siguiendo);
     }
@@ -173,12 +164,11 @@ function NoTelefonico() {
     var spName = "sp_obtenernoTelefonico";
     var Obj = {
         "spNombre": spName, "Campos": Campos, "Id": IdUser
-    }
+    };
     var data = JSON.stringify(Obj);
     ajax("OmniService.asmx", "Leer", data, "CargarNoTelefonico");
 }
 function CargarNoTelefonico(Response) {
-    console.warn(Response);
     for (var i = 0; i < Response.d.length; i++) {
         $("#noTelefonico").append('<div class="_500">' + Response.d[i].Datos.Telefono + '</div>');
     }
@@ -189,12 +179,11 @@ function TelefonoCasa() {
     var spName = "sp_obtenernoTelefonicoCasa";
     var Obj = {
         "spNombre": spName, "Campos": Campos, "Id": IdUser
-    }
+    };
     var data = JSON.stringify(Obj);
     ajax("OmniService.asmx", "Leer", data, "CargarTelefonoCasa");
 }
 function CargarTelefonoCasa(Response) {
-    console.warn(Response);
     for (var i = 0; i < Response.d.length; i++) {
         $("#noCasa").append('<div class="_500">' + Response.d[i].Datos.Telefono_Casa + '</div>');
     }
@@ -205,12 +194,11 @@ function Biografia() {
     var spName = "sp_obtenerBiografia";
     var Obj = {
         "spNombre": spName, "Campos": Campos, "Id": IdUser
-    }
+    };
     var data = JSON.stringify(Obj);
     ajax("OmniService.asmx", "Leer", data, "CargarBiografia");
 }
 function CargarBiografia(Response) {
-    console.warn(Response);
     for (var i = 0; i < Response.d.length; i++) {
         $("#biografia").append('<div>' + Response.d[i].Datos.Biografia + '</div >');
     }
@@ -222,24 +210,25 @@ function Publicacion() {
     
     var Obj = {
         "spNombre": spName, "Campos": Campos, "Id": IdUser
-    }
+    };
     var data = JSON.stringify(Obj);
     ajax("OmniService.asmx", "Leer", data, "CargarPublicaciones");
 }
 function CargarPublicaciones(Response) {
-    console.warn(Response);
     for (var i = 0; i < Response.d.length; i++) {
 
         //checar el tipo de la publicacion
-        if (Response.d[i].Publicacion.Tipo == 1) {
+        if (Response.d[i].Publicacion.Tipo === 1) {
             //Sin imagen
             $("#muroPrincipal").append('<div class="sl-item"> <div class="sl-left"> <img src="' + Response.d[i].Datos.Imagen_Perfil + '" class="img-circle"> </div>  <div class="sl-left"></div> <div class="sl-content"> <div class="sl-date text-muted"> ' + Response.d[i].Publicacion.Fecha_Publicacion + ' </div> <blockquote> <p>' + Response.d[i].Publicacion.Descripcion + '</p> <small>Alguien famoso <cite title="Source Title">' + Response.d[i].Publicacion.Fuente + '</cite></small> </blockquote> </div> </div>');
-        } else if (Response.d[i].Publicacion.Tipo == 2) {
+        } else if (Response.d[i].Publicacion.Tipo === 2) {
             //Con imagen
-            $("#muroPrincipal").append('<div class="sl-item"> <div class="sl-left"> <img src="' + Response.d[i].Datos.Imagen_Perfil + '" class="img-circle"> </div>  <div class="sl-left"> <img src="' + Response.d[i].Publicacion.Imagen + '" class="img-circle"> </div> <div class="sl-content"> <div class="sl-date text-muted"> ' + Response.d[i].Publicacion.Fecha_Publicacion + ' </div><blockquote> <p> ' + Response.d[i].Publicacion.Descripcion + '</p> <small>Alguien famoso <cite title="Source Title">' + Response.d[i].Publicacion.Fuente + '</cite></small> </blockquote> </div> </div>');
-        } else if (Response.d[i].Publicacion.Tipo == 3) {
+            $("#muroPrincipal").append('<div class="sl-item"> <div class="sl-left"> <img src="" class="img-circle"> </div>  <div class="sl-left"> <img src="' + Response.d[i].Publicacion.Imagen + '" class="img-circle"> </div> <div class="sl-content"> <div class="sl-date text-muted">  </div><blockquote> <p> ' + Response.d[i].Publicacion.Descripcion + '</p> <small>Alguien famoso <cite title="Source Title">' + Response.d[i].Publicacion.Fuente + '</cite></small> </blockquote> </div> </div>');
+            debugger;
+
+        } else if (Response.d[i].Publicacion.Tipo === 3) {
             //Con imagen y titulo
-            $("#muroPrincipal").append('<div class="sl-item"> <div class="sl-left"> <img src="' + Response.d[i].Datos.Imagen_Perfil + '" class="img-circle"> </div>  <div class="sl-left"> <img src="' + Response.d[i].Publicacion.Imagen + '" class="img-circle"> </div> <div class="sl-content"> <div class="sl-date text-muted"> ' + Response.d[i].Publicacion.Fecha_Publicacion + ' </div> <p>' + Response.d[i].Publicacion.Titulo + '</p> <blockquote> <p> ' + Response.d[i].Publicacion.Descripcion + ' </p> <small>Alguien famoso <cite title="Source Title">' + Response.d[i].Publicacion.Fuente + '</cite></small> </blockquote> </div> </div>');
+            $("#muroPrincipal").append('<div class="sl-item"><div class="sl-left"><img src="' + Response.d[i].Datos.Imagen_Perfil + '" class="img-circle"></div><div class="sl-content"><div class="sl-date text-muted">' + Response.d[i].Publicacion.Fecha_Publicacion + '</div><div class="sl-author"><a href="">' + Response.d[i].Publicacion.Titulo + '</a></div><div><p>' + Response.d[i].Publicacion.Descripcion + '</p><p><span class="inline w-lg w-auto-xs p-a-xs b dark-white"><img src="' + Response.d[i].Publicacion.Imagen + '" class="w-full"></span></p></div></div></div>');
         }
         $("#biografia").append('<div>' + Response.d[i].Datos.Biografia + '</div >');
     }
@@ -257,13 +246,12 @@ function ActualizarEstadoEnBD() {
 
         var Obj = {
             "spNombre": spName, "desNueva": Campos, "id": IdUser
-        }
+        };
         var data = JSON.stringify(Obj);
         ajax("OmniService.asmx", "Actualizar", data, "CargarActualizarEstadoEnBD");
     }
 }
 function CargarActualizarEstadoEnBD(Response) {
-    console.warn(Response);
     $("#estadoUsuario").empty();
     Estado();
     //Hacer que se regrese el div 
@@ -275,15 +263,12 @@ function Amigos() {
     var spName = "sp_obtenerPerfilAmigos";
     var Obj = {
         "spNombre": spName, "Campos": Campos, "Id": IdUser
-    }
+    };
     var data = JSON.stringify(Obj);
     ajax("OmniService.asmx", "Leer", data, "CargarAmigos");
 }
 function CargarAmigos(Response) {
-    console.warn(Response);
     for (var i = 0; i < Response.d.length; i++) {
-        $("#friends").append('<div class="col-xs-6 col-lg-4"><div class="list-item box r m-b" ><a herf="" class="list-left"><span class="w-40 avatar"><img src="' + Response.d[i].Datos.Imagen_Perfil + '" alt="..."> <i class="on b-white bottom"></i> </span></a><div class="list-body"><div class="text-ellipsis"><a href="">' + Response.d[i].Usuario.Nick + '</a></div><small class="text-muted text-ellipsis">' + Response.d[i].Datos.Vocacion + '</small> </div></div></div >');
+        $("#friends").append('<div class="col-xs-6 col-lg-4"><div class="list-item box r m-b" ><a herf="" class="list-left"><span class="w-40 avatar"><img src="'+Response.d[i].Datos.Imagen_Perfil+'" alt="..."> <i class="on b-white bottom"></i> </span></a><div class="list-body"><div class="text-ellipsis"><a href="">' + Response.d[i].Usuario.Nick + '</a></div><small class="text-muted text-ellipsis">' + Response.d[i].Datos.Vocacion + '</small> </div></div></div >');
     }
 }
-/*SPLISTO*/ // imagen del amigo, vocacion, nick
-//$("#muroPrincipal").append('<div class="sl-item"> <div class="sl-left"> <img src="../recursos/img/logo.png" class="img-circle"> </div> <div class="sl-content"> <div class="sl-date text-muted"> Sat, 5 Mar </div> <p>Preparar presentacion</p> <blockquote> <p> Los sentimientos son algo que simplemente no se pueden entender, y se cometen muchas errores por ellos. Pero es aquella la mente la que se encarga de aprender de estos errores </p> <small>Alguien famoso <cite title="Source Title">Fuente titulos</cite></small> </blockquote> </div> </div>');
